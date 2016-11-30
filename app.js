9ed6b808e71e90a7604f07478659c9e66dfecb95
takumi_kosakai <takumi.kosakai@fox-hound.jp>
2016-12-01 00:01:50 +0900

    feedの取得を自動化

--- app.js
+++ app.js
@@ -63,50 +63,50 @@ app.use(function(err, req, res, next) {
 ///////////////////////////////////////////////////////////
 ////////////////////テストコード/////////////////////////////
 ///////////////////////////////////////////////////////////
-var FeedParser = require('feedparser');  
-var request = require('request');  
+// var FeedParser = require('feedparser');  
+// var request = require('request');  
 
-async.parallel([
-	// rssテーブルとrss_categoryテーブルをJOINして抽出
-	function(callback) {
-		db.query('SELECT `rss`.id, `rss`.name, url, `rss_category`.name as category_name FROM `rss` LEFT JOIN `rss_category` ON `rss`.rss_category_id = `rss_category`.id', function(err,rows,fields) {
-			callback(err,rows);
-		});
-	}
-],function(err,rssList) {
-	if(err) throw err;
-	_.chain(rssList)
-	.first()
-	.each(function(rss){
-		// とりあえずrss毎に処理を繰り返す想定。INSERTだかUPDATEだかはrss毎に１発で流したい。
-		var feedparser = new FeedParser({});
-		var req = request(rss.url);
-		var items = [];
-		req.on('response', function(res) {
-			this.pipe(feedparser);
-		});
+// async.parallel([
+// 	// rssテーブルとrss_categoryテーブルをJOINして抽出
+// 	function(callback) {
+// 		db.query('SELECT `rss`.id, `rss`.name, url, `rss_category`.name as category_name FROM `rss` LEFT JOIN `rss_category` ON `rss`.rss_category_id = `rss_category`.id', function(err,rows,fields) {
+// 			callback(err,rows);
+// 		});
+// 	}
+// ],function(err,rssList) {
+// 	if(err) throw err;
+// 	_.chain(rssList)
+// 	.first()
+// 	.each(function(rss){
+// 		// rss毎に処理を繰り返し。SQLはrss毎に１発で流す。
+// 		var feedparser = new FeedParser({});
+// 		var req = request(rss.url);
+// 		var items = [];
+// 		// 情報取得
+// 		req.on('response', function(res) {
+// 			this.pipe(feedparser);
+// 		});
+// 		// 配列に詰める
+// 		feedparser.on('readable', function() {  
+// 		  while(item = this.read()) {
+// 		    items.push(item);
+// 		  }
+// 		});
 
-		feedparser.on('readable', function() {  
-		  while(item = this.read()) {
-		    // console.log(item);
-		    items.push(item);
-		  }
-		});
-		// この後SQLを流すことになる。一旦比較は考えない
-		feedparser.on('end', function() {
-			// 挿入するデータの整形
-			var feeds = _.map(items, function(item) {
-				return [rss.id,item.title,item.link,rss.category_name,item.date,new Date()];
-			});
-		 	// 同じurlのfeedがあれば更新を行い、なければ挿入する
-		 	db.query('INSERT INTO `feed` (`rss_id`, `title`, `url`, `category`, `date`, `update_time`) VALUES' + db.escape(feeds) + 
-		 		'ON DUPLICATE KEY UPDATE `title` = VALUES(`title`), `date` = VALUES(`date`), `update_time` = VALUES(`update_time`)',function(err,rows){
-		      console.log(err,rows);
-		    });
-			console.log('-----');
-		});
-	})
-	.value();
-})
+// 		feedparser.on('end', function() {
+// 			// 挿入するデータの整形
+// 			var feeds = _.map(items, function(item) {
+// 				return [rss.id,item.title,item.link,rss.category_name,item.date,new Date()];
+// 			});
+// 		 	// 同じurlのfeedがあれば更新を行い、なければ挿入する
+// 		 	db.query('INSERT INTO `feed` (`rss_id`, `title`, `url`, `category`, `date`, `update_time`) VALUES' + db.escape(feeds) + 
+// 		 		'ON DUPLICATE KEY UPDATE `title` = VALUES(`title`), `date` = VALUES(`date`), `update_time` = VALUES(`update_time`)',function(err,rows){
+// 		      console.log(err,rows);
+// 		    });
+// 			console.log('-----');
+// 		});
+// 	})
+// 	.value();
+// });
 
 module.exports = app;
